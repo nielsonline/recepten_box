f921a13add8d33e017bc110af3978962ac87c426
nielsonline <nielsettema95@gmail.com>
2016-12-09 13:08:30 +0100

    fixed the routing, added the links, listing all recipes.

--- app/controllers/recepts_controller.rb
+++ app/controllers/recepts_controller.rb
@@ -2,6 +2,7 @@ class ReceptsController < ApplicationController
   before_action :find_recepts, only: [:show, :edit, :update, :destroy]
 
   def index
+    @recepts = Recept.all.order("created_at DESC")
   end
 
   def show
@@ -21,13 +22,29 @@ class ReceptsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recept.update(recept_params)
+      redirect_to @recept
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recept.destroy
+    redirect_to root_path, notice: "Succesfully deleted the recipe."
+  end
+
   private
 
   def recept_params
     params.require(:recept).permit(:title, :description)
   end
 
-  def find_recept
+  def find_recepts
     @recept = Recept.find(params[:id])
   end
 end
