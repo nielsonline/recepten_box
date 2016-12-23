9d81ec3f3281347cb8b895838900f802344b051b
Niels Ettema <nielsettema95@gmail.com>
2016-12-23 19:42:11 +0100

    added the hovering images and some colours, front end changes
    bootstrap sass

--- app/models/recept.rb
+++ app/models/recept.rb
@@ -1,4 +1,4 @@
 class Recept < ApplicationRecord
-  has_attached_file :image, styles: { medium: "400x400>" }
+  has_attached_file :image, styles: { medium: "250x250>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
